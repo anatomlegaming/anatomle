33473c200099596dbba235ab3b42106e6e262c2d
anatomlegaming <ilovethrowingaway@gmail.com>
2026-02-20 11:48:32 +1100

    Update footEngine.js

--- js/3d/footEngine.js
+++ js/3d/footEngine.js
@@ -130,6 +130,7 @@ window.addEventListener('DOMContentLoaded', function() {
         cam.position.set(center.x, center.y+size.y*0.2, center.z+Math.abs(maxDim/2/Math.tan(fov/2))*1.2);
         cam.lookAt(center); ctrl.update();
         window.reset3D();
+        window.dispatchEvent(new CustomEvent('modelReady'));
     });
 
     function animate(){ requestAnimationFrame(animate); ctrl.update(); renderer.render(scene,cam); }
